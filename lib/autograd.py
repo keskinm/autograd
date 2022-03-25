6f3dcd0b747f978ce3934cf8c7813d4db6028ed0
E044382 <sasha.keskin_ext@michelin.com>
2022-03-25 13:38:06 +0100

    Add power, divide, mul/matmul, neg

--- lib/autograd.py
+++ lib/autograd.py
@@ -1,4 +1,5 @@
 import uuid
+import numpy as np
 
 from lib.active_graph import active_graph
 
@@ -19,6 +20,8 @@ class Execution:
                 continue
             grads = obj.backward(obj.grad)
             for inp, grad in zip(obj.inputs, grads):
+                if isinstance(obj, Constant):
+                    continue
                 if not inp.obj_id in vis:
                     inp.grad = grad
                     vis.add(inp.obj_id)
@@ -66,34 +69,69 @@ class InGraphObject:
         self.obj_id = obj_id or str(uuid.uuid4())
         self.name = name
 
-    def __mul__(self, other):
+    @staticmethod
+    def check_other(other):
         if not isinstance(other, Tensor):
-            other = Tensor(value=other)
+            if isinstance(other, float) or isinstance(other, int):
+                other = Constant(value=other)
+            else:
+                other = Tensor(value=other)
+        return other
+
+    def __matmul__(self, other):
+        other = self.check_other(other)
+        return MatMul(self, other)
+
+    def __mul__(self, other):
+        other = self.check_other(other)
         return Mul(self, other)
 
+    def __pow__(self, other):
+        other = self.check_other(other)
+        return Power(self, other)
+
+    def __div__(self, other):
+        other = self.check_other(other)
+        return Divide(self, other)
+
+    def __neg__(self):
+        return Mul(self, Constant(-1))
+
     def __add__(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor(value=other)
+        other = self.check_other(other)
         return Add(self, other)
 
 class Tensor(InGraphObject):
     def __init__(self, value, name=None):
         InGraphObject.__init__(self, name=name)
         active_graph[-1].tensors[self.obj_id] = self
-        self.value = value
+        self._value = value
         self.grad = 0
 
     def reset_grad(self):
         self.grad = 0
 
     def __call__(self, *args, **kwargs):
+        return self._value
+
+class Constant(Tensor):
+    def __init__(self, value, name=None):
+        super().__init__(value, name=name)
+
+    @property
+    def value(self):
         return self.value
+
+    @value.setter
+    def value(self, value):
+        raise ValueError("Cannot reassign constant")
 
 class Operation(InGraphObject):
     def __init__(self, name=None):
         InGraphObject.__init__(self, name=name)
         active_graph[-1].ops[self.obj_id] = self
         self.value = None
+        self.grad = 0
 
     def forward(self):
         raise NotImplementedError
@@ -119,14 +157,79 @@ class Add(Operation):
         return self.a() + self.b()
 
     def backward(self, dout):
-        return self.b() * dout, self.a() * dout
+        return dout, dout
 
     def __call__(self, *args, **kwargs):
         return self.value
 
+class Power(Operation):
+    def __init__(self, a, b):
+        Operation.__init__(self, 'add')
+        self.a = a
+        self.b = b
+
+    @property
+    def inputs(self):
+        return [self.a, self.b]
+
+    def forward(self):
+        return np.power(self.a(), self.b())
+
+    def backward(self, dout):
+        a = self.a()
+        b = self.b()
+        return dout*b*np.power(a, (b-1)), dout*np.log(a)*np.power(a, b)
+
+    def __call__(self, *args, **kwargs):
+        return self.value
+
+class Divide(Operation):
+    def __init__(self, a, b):
+        Operation.__init__(self, 'add')
+        self.a = a
+        self.b = b
+
+    @property
+    def inputs(self):
+        return [self.a, self.b]
+
+    def forward(self):
+        return self.a() / self.b()
+
+    def backward(self, dout):
+        a = self.a()
+        b = self.b()
+
+        return dout/b, dout*a/np.power(b, 2)
+
+    def __call__(self, *args, **kwargs):
+        return self.value
+
+
 class Mul(Operation):
     def __init__(self, a, b):
         Operation.__init__(self, 'mul')
+        self.a = a
+        self.b = b
+
+    @property
+    def inputs(self):
+        return [self.a, self.b]
+
+    def forward(self):
+        res =  self.a() * self.b()
+        return res
+
+    def backward(self, dout):
+        return dout * self.b(), dout * self.a()
+
+    def __call__(self, *args, **kwargs):
+        return self.value
+
+
+class MatMul(Operation):
+    def __init__(self, a, b):
+        Operation.__init__(self, 'matmul')
         self.a = a
         self.b = b
 
--- test_autograd.py
+++ test_autograd.py
@@ -1,15 +1,13 @@
-from lib.autograd import Graph, Tensor, Execution
-
-import numpy as np
+from lib.autograd import Graph, Tensor, Execution, Constant
 
 
 
 def example1():
-  val1, val2, val3 = np.array([0.9]), np.array([0.4]), np.array([1.3])
+  val1, val2, val3 = 0.9, 0.4, 1.3
   with Graph() as g:
     x = Tensor(val1, name='x')
     y = Tensor(val2, name='y')
-    c = Tensor(val3, name='c')
+    c = Constant(val3, name='c')
 
     z = (x*y+c)*c + x
     path = g.compute_path(z.obj_id)
@@ -18,13 +16,6 @@ def example1():
     assert z.value == (val1*val2+val3)*val3+val1
     executor.backward_ad()
 
-    print([x.grad for x in executor.path])
-    x_actual_expected = [g.tensors[x.obj_id].grad, val3*val2+1]
-    y_actual_expected = [g.tensors[y.obj_id].grad, val1*val3]
-    c_actual_expected = [g.tensors[c.obj_id].grad, val1*val2+2*val3]
-
-    print(x_actual_expected)
-    print(y_actual_expected)
-    print(c_actual_expected)
-
-example1()
+    assert g.tensors[x.obj_id].grad == val3*val2+1
+    assert g.tensors[y.obj_id].grad == val1*val3
+    assert g.tensors[c.obj_id].grad == val1*val2+2*val3
