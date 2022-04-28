c964cfec6bdcf5afe185394991d5999bc87f2d8f
E044382 <sasha.keskin_ext@michelin.com>
2022-04-28 14:14:46 +0200

    Some refactoring.

--- models/conv_neural_network.py
+++ models/conv_neural_network.py
@@ -12,7 +12,7 @@ class ConvNeuralNetwork:
     def __init__(self):
         self.X = None
         self.y = None
-        self.W = None
+        self.W = {}
         self.loss = None
         self.losses = []
 
@@ -26,14 +26,14 @@ class ConvNeuralNetwork:
             y.append([_x, _y])
 
         self.X, self.y = np.array(X), np.array(y)
-        self.W1 = np.random.normal(0, size=[self.X.shape[1]//3, self.X.shape[2]//3])
-        self.W2 = np.random.normal(0, size=[self.X.shape[1]//3, self.X.shape[2]//3])
+        self.W['1'] = np.random.normal(0, size=[self.X.shape[1]//3, self.X.shape[2]//3])
+        self.W['2'] = np.random.normal(0, size=[self.X.shape[1]//3, self.X.shape[2]//3])
 
         flattened_size = (self.X.shape[1] - self.X.shape[1]//3) * (self.X.shape[2] - self.X.shape[2]//3)
 
-        self.W_x_pred = np.random.normal(0, size=flattened_size)
-        self.W_y_pred = np.random.normal(0, size=flattened_size)
-        self.W_xy_pred = np.random.normal(0, size=[2, flattened_size])
+        self.W['x_pred'] = np.random.normal(0, size=flattened_size)
+        self.W['y_pred'] = np.random.normal(0, size=flattened_size)
+        self.W['xy_pred'] = np.random.normal(0, size=[2, flattened_size])
 
     def make_dataset_for_classification(self):
         digits = datasets.load_digits()
@@ -46,16 +46,16 @@ class ConvNeuralNetwork:
                 with Graph() as g:
                     W1, W2, W_xy_pred, executor = self.forward_stochastic(X_sample, g, y_sample)
                     executor.backward_ad()
-                    self.W1 = self.W1 - 0.001 * W1.grad
-                    self.W2 = self.W2 - 0.001 * W2.grad
-                    self.W_xy_pred = self.W_xy_pred - 0.001 * W_xy_pred.grad
+                    self.W['1'] -= 0.001 * W1.grad
+                    self.W['2'] -= 0.001 * W2.grad
+                    self.W['xy_pred'] -= 0.001 * W_xy_pred.grad
 
     def forward_stochastic(self, X_sample, g, y_sample):
         X = Tensor(X_sample, name='X')
         y = Tensor(y_sample, name='y')
-        W1 = Tensor(self.W1, name='W1')
-        W2 = Tensor(self.W2, name='W2')
-        W_xy_pred = Tensor(self.W_xy_pred, name='W_xy_pred')
+        W1 = Tensor(self.W['1'], name='W1')
+        W2 = Tensor(self.W['2'], name='W2')
+        W_xy_pred = Tensor(self.W['xy_pred'], name='W_xy_pred')
         z1 = BatchLessConv2D(X, W1, compute_grad=[W1.id])
         z2 = BatchLessConv2D(z1, W2, compute_grad=[W2.id])
         z3 = Flatten(z2)
@@ -78,8 +78,8 @@ class ConvNeuralNetwork:
                 with Graph() as g:
                     X = Tensor(X_sample, name='X')
                     y = Tensor(y_sample, name='y')
-                    W1 = Tensor(self.W1, name='W1')
-                    W2 = Tensor(self.W2, name='W2')
+                    W1 = Tensor(self.W['1'], name='W1')
+                    W2 = Tensor(self.W['2'], name='W2')
 
                     z11 = BatchLessConv2D(X, W1, compute_grad=[W1.id])
                     z21 = BatchLessConv2D(z11, W2, compute_grad=[W2.id])
@@ -89,8 +89,8 @@ class ConvNeuralNetwork:
                     z22 = BatchLessConv2D(z12, W2, compute_grad=[W2.id])
                     z32 = Flatten(z22)
 
-                    W_x_pred = Tensor(self.W_x_pred, name='W_x_pred')
-                    W_y_pred = Tensor(self.W_y_pred, name='W_y_pred')
+                    W_x_pred = Tensor(self.W['x_pred'], name='W_x_pred')
+                    W_y_pred = Tensor(self.W['y_pred'], name='W_y_pred')
                     x_pred = Dot(z31, W_x_pred, compute_grad=[W_x_pred.id])
                     y_pred = Dot(z32, W_y_pred, compute_grad=[W_y_pred.id])
                     xy_pred = Stack([x_pred, y_pred])
@@ -102,7 +102,7 @@ class ConvNeuralNetwork:
                     executor.forward()
                     print("xy_pred", xy_pred(), "xy_real", y(), "loss", loss())
                     executor.backward_ad()
-                    self.W1 = self.W1 - 0.001 * W1.grad
-                    self.W2 = self.W2 - 0.001 * W2.grad
-                    self.W_x_pred = self.W_x_pred - 0.001 * W_x_pred.grad
-                    self.W_y_pred = self.W_y_pred - 0.001 * W_y_pred.grad
+                    self.W['1'] -= 0.001 * W1.grad
+                    self.W['2'] -= 0.001 * W2.grad
+                    self.W['x_pred'] = self.W['x_pred'] - 0.001 * W_x_pred.grad
+                    self.W['y_pred'] = self.W['y_pred'] - 0.001 * W_y_pred.grad
